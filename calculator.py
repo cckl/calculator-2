d80d7500d5695f2fc44677eb8ff42ca2b5e9723e
Hackbright Student <no-reply@hackbrightacademy.com>
2019-01-09 17:26:44 -0800

    with Try and Except

--- calculator.py
+++ calculator.py
@@ -12,29 +12,32 @@ from arithmetic import add, subtract, multiply, divide, square, cube, mod, power
 
 # Your code goes here
 while True:
-    arithmetic_operation = input(">")
-    tokens = arithmetic_operation.split(" ")
-    if tokens[0] == "q":
-        break
-    token_one = int(tokens[1])
-    if len(tokens) > 2:
-        token_two = int(tokens[2])
-        if tokens[0] == "+":
-            print(add(token_one, token_two))
-        elif tokens[0] == "-":
-            print(subtract(token_one, token_two))
-        elif tokens[0] == "*":
-            print(multiply(token_one, token_two))
-        elif tokens[0] == "/":
-            print(divide(token_one, token_two))
-        elif tokens[0] == "mod":
-            print(mod(token_one, token_two))
-        elif tokens[0] == "pow":
-            print(power(token_one, token_two))    
-    elif tokens[0] == "square":
-        print(square(token_one))
-    elif tokens[0] == "cube":
-        print(cube(token_one))
+    try:
+        arithmetic_operation = input(">")
+        tokens = arithmetic_operation.split(" ")
+        if tokens[0] == "q":
+            break
+        token_one = int(tokens[1])
+        if len(tokens) > 2:
+            token_two = int(tokens[2])
+            if tokens[0] == "+":
+                print(add(token_one, token_two))
+            elif tokens[0] == "-":
+                print(subtract(token_one, token_two))
+            elif tokens[0] == "*":
+                print(multiply(token_one, token_two))
+            elif tokens[0] == "/":
+                print(divide(token_one, token_two))
+            elif tokens[0] == "mod":
+                print(mod(token_one, token_two))
+            elif tokens[0] == "pow":
+                print(power(token_one, token_two))    
+        elif tokens[0] == "square":
+            print(square(token_one))
+        elif tokens[0] == "cube":
+            print(cube(token_one))
+    except IndexError:
+        print("invalid token")       
     
 
 
